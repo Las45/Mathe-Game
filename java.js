45dbe9e389cf03cbd382abb7113e7267155d55e5
Las45 <matteo.morent@gmail.com>
2026-04-23 10:53:28 +0200

    Added theoreticly the Operators

--- java.js
+++ java.js
@@ -1,4 +1,19 @@
 var round = prompt("Wie viele Runden möchtest du spielen?");
 var currentRound = 1;
+var operator;
 
-
+function set_plus(){
+    operator = "+"
+}
+function set_minus(){
+    operator = "-"
+}
+function set_random(){
+    var operator_rand = Math.floor(Math.random() * 2)
+    if(operator_rand == 1){
+        operator = "+"
+    }
+    else if(operator_rand == 2){
+        operator = "-"
+    }
+}
